14c07a5b3b5d86c20fc67882a222200c3d785305
Santiago Graviano <santiago.graviano@gmail.com>
2017-03-25 18:40:57 -0300

    comments in search function

--- search/search.h
+++ search/search.h
@@ -1,5 +1,10 @@
 #include "algorithms/linear.h"
-
+/*
+	Searches for a value in the linked list, using the selected algorithm,
+	and returns its position
+	@arguments (int value, Node *list)
+	@return int
+*/
 int search (int value, Node *list, int algorithm)
 {
   switch (algorithm) {
