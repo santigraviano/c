3fcd9505f788d52a9e0b34505c17cbc26ec99143
Santiago Graviano <santiago.graviano@gmail.com>
2017-03-25 18:39:21 -0300

    search function deleted, use search moduel instead

--- llist/xtdllist.h
+++ llist/xtdllist.h
@@ -4,38 +4,6 @@
   This header offers extended functionalities for linked lists
 */
 
-/*
-	Searches for a value in the linked list
-	and returns its position
-	@arguments (int value, Node *list)
-	@return int
-*/
-int search (int value, Node *list)
-{
-	int position = 0;
-	if (list->value == value)
-	{
-		return position;
-	}
-
-	while (list->next != NULL)
-	{
-		if (list->value == value)
-		{
-			return position;
-		}
-		position++;
-		list = list->next;
-	}
-
-	if (list->value == value)
-	{
-		return position;
-	}
-
-	return -1;
-}
-
 int printList (Node *list)
 {
 	if (!empty(list))
