91896977e4b0ea739e158fcd7793fbf3f7e2e4b6
Santiago Graviano <santiago.graviano@gmail.com>
2017-03-25 18:03:04 -0300

    compare lists function added

--- llist/xtdllist.h
+++ llist/xtdllist.h
@@ -55,3 +55,20 @@ int printList (Node *list)
 
 	return 0;
 }
+
+/*
+	Compares two linked lists with even number of nodes
+	@arguments (Node *listA, Node *listB)
+	@return (int) number of different node values
+*/
+
+int compare (Node *listA, Node *listB)
+{
+	int diffs = 0;
+	for (Node *a = listA, *b = listB; a != NULL && b != NULL; a = a->next, b = b->next)
+	{
+		if (a->value != b->value)
+			diffs++;
+	}
+	return diffs;
+}
